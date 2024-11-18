67946b947b95a62449504e5ce2c77564335d5f04
dorwp <dorianwill119@gmail.com>
2024-11-18 18:47:31 -0500

    Part1 Complete

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,28 @@
 {
     public class Program
     {
+
+        public static void InfoGather()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite code language?");    
+            string favLanguage = Console.ReadLine();
+            
+            Console.WriteLine($"Hello {name}, Your favorite color is {favColor}, your favorite animal is a {favAnimal}," +
+                              $" and your favorite language is {favLanguage}!");
+        }
+        
         static void Main(string[] args)
         {
-            
+            InfoGather();
             
             
             
